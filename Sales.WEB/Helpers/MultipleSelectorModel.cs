bb11081e199d8845fc8a90d9ada867d1edd77446
Jhonatan Sebastian Soto Fernandez <113725550+JhonatanDev18@users.noreply.github.com>
2023-03-25 17:47:43 -0500

    product creation

--- Sales.WEB/Helpers/MultipleSelectorModel.cs
+++ Sales.WEB/Helpers/MultipleSelectorModel.cs
@@ -2,5 +2,14 @@
 {
     public class MultipleSelectorModel
     {
+        public MultipleSelectorModel(string key, string value)
+        {
+            Key = key;
+            Value = value;
+        }
+
+        public string Key { get; set; }
+
+        public string Value { get; set; }
     }
 }
